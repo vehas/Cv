c0e6ba5d0a06c4f170bf8c093ea599b6322efc2f
vehas <sky.48478@gmail.com>
2014-11-16 07:10:24 +0700

    find biggest contour

--- Test/main.cpp
+++ Test/main.cpp
@@ -7,6 +7,8 @@
 
 using namespace cv;
 using namespace std;
+typedef vector<vector<Point> > contour;
+typedef vector<Vec4i> hierarchy;
 Mat showCanny(Mat input){
 	Mat graySrc, outCanny;
 
@@ -25,29 +27,40 @@ Mat showCanny(Mat input){
 	int th1 = min1, th2 = 20, key;
 	bool loop=true;
 	namedWindow("canny", 1);
-	createTrackbar("CannyT1", "canny", &th1, max1);
-	createTrackbar("CannyT2", "canny", &th2, max2);
+	createTrackbar("cannyT1", "canny", &th1, max1);
+	createTrackbar("cannyT2", "canny", &th2, max2);
 	//Mat canny_output;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
+	contour contours;
+	hierarchy hierarchy;
 	RNG rng(12345);
 	Mat drawing = Mat::zeros(input.size(), CV_8UC3);
 
 	while (loop){
 		
-		cv::Canny(graySrc, outCanny, th1, th2);
+		cv::Canny(graySrc, outCanny, th1, th1*3);
 
-		//GaussianBlur(outCanny, outCanny, Size(3,3), 0, 0);
+		GaussianBlur(outCanny, outCanny, Size(3,3), 0, 0);
 		imshow("canny", outCanny);
 		
 		findContours(outCanny, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+		
+		//find outter contour
+		int contourNum = contours.size();
+		contour big;
+		for (int i = 0; i < contourNum; i++){
+			if (hierarchy[i][3] < 0)
+				big.push_back(contours[i]);
+		}
+
+		//end find outter contour
+
 
 		/// Draw contours
 		drawing = Mat::zeros(input.size(), CV_8UC3);
-		for (int i = 0; i< contours.size(); i++)
+		for (int i = 0; i< big.size(); i++)
 		{
 			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+			drawContours(drawing, big, i, color, 2, 8, hierarchy, 0, Point());
 		}
 
 		imshow("Contours", drawing);
@@ -60,6 +73,8 @@ Mat showCanny(Mat input){
 		case 'a': if (th2 >= min1){ th2 -= min2; cout << th2 << endl; } break;
 		case 'o': loop=!loop;
 		}
+		setTrackbarPos("cannyT1", "canny", th1);
+		setTrackbarPos("cannyT2", "canny", th2);
 		Sleep(300);
 
 	}
@@ -67,18 +82,40 @@ Mat showCanny(Mat input){
 	return outCanny;
 }
 
+
+
 int main(){
 	Mat input_image, graySrc,outCanny,outGauss;
-	input_image = imread("c:\\opencv\\paint.jpg"); //path to image
+	input_image = imread("c:\\opencv\\p1.jpg"); //path to image
 
 	imshow("Hello OpenCV!", input_image);
 	
 	cvtColor(input_image, graySrc, CV_BGR2GRAY);
 
-	GaussianBlur(graySrc, outGauss, Size(3,3), 0, 0);
+	GaussianBlur(graySrc, outGauss, Size(4,4), 0, 0);
 	imshow("GaussianBlur", outGauss);
-	
-	outCanny =  showCanny(outGauss);
+	/*  //  adaptiveThreshold
+	Mat outAts;
+	adaptiveThreshold(outGauss, outAts, 255, ADAPTIVE_THRESH_GAUSSIAN_C,
+		THRESH_BINARY, 11, 2);
+		imshow("adaptiveThreshold", outAts);
+
+		*/
+
+	/// Apply the dilation operation
+	int dilation_type = MORPH_RECT, dilation_size = 3;
+	Mat element = getStructuringElement(dilation_type,
+		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+		Point(dilation_size, dilation_size));
+	Mat outDilate;
+	dilate(outGauss,outDilate, element);
+	imshow("Dilation Demo", outDilate);
+	// 
+	Mat outErode;
+	erode(outDilate, outErode, element);
+	imshow("Dilation Demo", outErode);
+
+	outCanny =  showCanny(outErode);
 	
 	
 	waitKey(0);
